284254a6a481088ade5be0d44b2869378fca1e2a
jihoon <lanslot84@gmail.com>
2023-11-02 07:25:38 +0900

    FEAR : bcrypt를 사용한 hash된 비밀번호와 role 저장하는 기능 추가

--- routers/signup.js
+++ routers/signup.js
@@ -17,18 +17,19 @@ router.get('/', (req, res, next) => {
  * 회원가입 요청
  */
 router.post("/", async (req, res, next) => {
-    const { userName, email, password } = req.body;
+    const { userName, email, password, role} = req.body;
     try {
-        const result = await userService.Signup({userName, email, password});
-        if(!result) {
-            throw new Error('이미 가입한 이메일입니다.');
+        const result = await userService.Signup({userName, email, password, role});
+        if(result.message == "DUPLICATED") {
+             throw new Error('이미 가입한 이메일입니다.');
+        } else if(result.message == "SUCCESS") {
+            res.status(201);
+            res.json(
+                {
+                    message: "회원가입에 성공했습니다.",
+                    user: result.user,
+                });
         }
-        res.status(201);
-        res.json(
-            {
-                message: "회원가입에 성공했습니다.",
-                user: result
-            });
     } catch(err) {
         res.status(400);
         res.json(
--- services/user-service.js
+++ services/user-service.js
@@ -1,17 +1,22 @@
+import bcrypt from 'bcryptjs';
+import passport from 'passport';
 
 import { User } from '../db/models/users/user-model.js';
 
 export default class UserService {
 
-    async Signup({userName, email, password}) {
-        const newUser = {userName, email, password};
+    async Signup({userName, email, password, role = "USER"}) {
+        const user = {userName, email, password, role};
         try {
-            const existUser = User.findOne({email: newUser.email});
-            if(existUser) {
-                return null;
-            }
-            const user = await User.create(newUser);
-            return user;
+            const existUser = await User.findOne( {email: user.email} );
+            if(existUser!==null) {
+                return { message : "DUPLICATED"};
+            } 
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(user.password, salt);
+            user.password = hash;
+            const newUser = await User.create(user);
+            return { message : "SUCCESS", user: newUser};
         } catch(err) {
             return err;
         }
