3438ac1f763783cba1868ad2db9aa3769fbe3005
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-01 17:35:30 +0900

    feat:router추가

--- routers/carup.js
+++ routers/carup.js
@@ -1,13 +1,73 @@
 import { Router } from 'express';
-//상품 등록
-
+import { Car } from '../db/models/cars/cars-model';
+import CarService from '../services/car-service';
+const carService = new CarService;
 const router = Router();
 
-router.get('/', (req, res, next) => {
-    res.send("carup router");
-})
+router.get('/:carId', async (req, res, next) => {
+    const carId = req.params.carId;
+    try {
+        const car = await Car.findById(carId);
+        if (!car) {
+            return res.status(404).json({ message: 'Car not found' });
+        }
+        res.status(200).json(car);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error retrieving car' });
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    const { carName, carPrice, img, speed, mileage, option, category } = req.body;
+    try {
+        const result = await carService.addNewCar({ carName, carPrice, img, speed, mileage, option, category });
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error creating car' });
+    }
+});
+
+
+router.put('/:carId', async (req, res, next) => {
+    const carId = req.params.carId;
+    const { carName, carPrice, img, speed, mileage, option, category } = req.body;
+    try {
+        const carToUpdate = await Car.findOne({ _id: carId });
+        if (!carToUpdate) {
+            return res.status(404).json({ message: 'Car not found' });
+        }
+        carToUpdate.carName = carName;
+        carToUpdate.carPrice = carPrice;
+        carToUpdate.img = img;
+        carToUpdate.speed = speed;
+        carToUpdate.mileage = mileage;
+        carToUpdate.option = option;
+        carToUpdate.category = category;
+        await carToUpdate.save();
+
+        res.status(200).json({ message: 'Car updated successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error updating car' });
+    }
+});
+
+router.delete('/:carId', async (req, res, next) => {
+    const carId = req.params.carId;
+    try {
+        const car = await Car.findByIdAndDelete(carId);
+        if (!car) {
+            return res.status(404).json({ message: 'Car not found' });
+        }
+        res.status(200).json({ message: 'Car deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error deleting car' });
+    }
+});
 
 const result = `Server is working`;
 
 export default router;
-export {result};
