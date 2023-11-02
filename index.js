75cbcc277a05c69aaa1ab5ad14f9f2445b565a57
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-02 13:10:09 +0900

    feat:index파일수정

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 import express from 'express';
-import signupRouter from './routers/signup.js';
-import {result} from './routers/signup.js';
+import carRouter from './routers/carRouter.js';
+import cors from 'cors';
+import { config } from 'dotenv';
 
-//require("dotenv").config();
-import { config } from "dotenv";
-import { env } from "process";
-config();
-const { MONGO_NAME, MONGO_PASSWORD } = env;
+config(); // dotenv 설정 호출
+
+const MONGO_NAME = process.env.MONGO_NAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 import mongoose from 'mongoose';
-
 
 const port = 3000;
 const app = express();
 
 mongoose
   .connect(
-    `mongodb+srv://${MONGO_NAME}:${MONGO_PASSWORD}@project-db.duhldeb.mongodb.net/?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'elice-motors' })
-  .then(() => console.log("Connected to MongoDB"))
+    `mongodb+srv://${MONGO_NAME}:${MONGO_PASSWORD}@project-db.duhldeb.mongodb.net/elice-motors?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res, next) => {
-    res.send(result);
-});
-
-app.use(cors());
+// 상품 관련 CRUD 라우터를 사용
+app.use(
+  cors({
+    origin: '*', // 모든 도메인에서 요청 허용
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/signup", signupRouter);
+app.use('/car', carRouter); // '/car' 경로로 API 엔드포인트 사용
 
 app.listen(port, () => {
-    console.log(
-      `서버가 정상적으로 시작되었습니다. 주소: http://localhost:${port}`
-    );
+  console.log(`서버가 정상적으로 시작되었습니다. 주소: http://localhost:${port}`);
 });
