926ba4a6683c573b0999fa9a64dc03e3ffac72e6
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-02 15:35:02 +0900

    Docs:categoryRouter

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { Car } from '../db/models/cars/cars-model';
+import { Category } from '../db/models/category/category-model'
 import CarService from '../services/car-service';
 const carService = new CarService;
 const router = Router();
@@ -7,24 +7,26 @@ const router = Router();
 router.get('/:carId', async (req, res, next) => {
     const carId = req.params.carId;
     try {
-        const car = await Car.findById(carId);
-        if (!car) {
+        const category = await Category.findById(carId);
+        if (!category) {
             return res.status(404).json({ message: 'Car not found' });
         }
-        res.status(200).json(car);
+        res.status(200).json(category);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error retrieving category' });
+        res.status(err.status);
+        res.json({ message: err.message });
     }
 });
 router.post('/', async (req, res, next) => {
     const { carId, carType } = req.body;
     try {
         const result = await carService.addNewCar({ carId, carType });
-        res.status(201).json({message:'category creating success'});
+        res.status(201).json({message:'category creating success',result});
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error creating category' });
+        res.status(err.status);
+        res.json({ message: err.message });
     }
 });
 
@@ -32,7 +34,7 @@ router.put('/:carId', async (req, res, next) => {
     const carId = req.params.carId;
     const { carType } = req.body; //id는 수정안함
     try {
-        const carToUpdate = await Car.findOne({ _id: carId });
+        const carToUpdate = await Category.findOne({ _id: carId });
         if (!carToUpdate) {
             return res.status(404).json({ message: 'Category not found' });
         }
@@ -42,21 +44,23 @@ router.put('/:carId', async (req, res, next) => {
         res.status(200).json({ message: 'Category updated successfully' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error updating category' });
+        res.status(err.status);
+        res.json({ message: err.message });
     }
 });
 
 router.delete('/:carId', async (req, res, next) => {
     const carId = req.params.carId;
     try {
-        const car = await Car.findByIdAndDelete(carId);
-        if (!car) {
+        const category = await Category.findByIdAndDelete(carId);
+        if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
         res.status(200).json({ message: 'Category deleted successfully' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error deleting category' });
+        res.status(err.status);
+        res.json({ message: err.message });
     }
 });
 
