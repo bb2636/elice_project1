081cf4feccf043c8cb351933f47645d830ca9da8
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-02 11:50:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-//import carRouter from './routers/carRouter.js';
+import carRouter from './routers/carRouter.js';
 import userRouter from './routers/user-router.js';
 import cors from 'cors';
 import { config } from 'dotenv';
@@ -9,7 +9,7 @@ config(); // dotenv 설정 호출
 const MONGO_NAME = process.env.MONGO_NAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const port = 3000;
 const app = express();
@@ -22,6 +22,13 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
+// 상품 관련 CRUD 라우터를 사용
+app.use(
+  cors({
+    origin: '*', // 모든 도메인에서 요청 허용
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+  })
+);
 
 /* user router */
 app.use("/users", userRouter);
@@ -29,9 +36,10 @@ app.use("/users", userRouter);
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use('/car', carRouter); // '/car' 경로로 API 엔드포인트 사용
 
 app.listen(port, () => {
-  console.log(`server is running in ${port}`);
+  console.log(`서버가 정상적으로 시작되었습니다. 주소: http://localhost:${port}`);
 });
 
