da5e655c15968a59a5324bc7c4bfc0ff4303599e
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-03 15:57:10 +0900

    feat:전체상품조회추가

--- services/car-service.js
+++ services/car-service.js
@@ -1,19 +1,34 @@
 import { Car } from '../db/models/cars/cars-model.js';
 
 export default class CarService {
+    //상품 등록
     async CarUp({carName, carPrice, img,speed,mileage,fuel,carId,option,category}) {
         const car = await Car.create({carName, carPrice, img,speed,mileage,fuel,carId,option,category});
         try{
             if(!carName || !carPrice || !img || !speed || !mileage || !fuel || !carId){
-                return {message : "Missing_Field"}
+                return {message : "MISSING_FIELD"}
             }
             const existCar = await Car.findOne({carId: car.carId});
             if(existCar != null){
-                return {message: "Duplicated"}; //carId중복
+                return {message: "DUPLICATED"}; //carId중복
             }
             const newCar = await Car.create(car);
-            return {message : "Success", car: newCar};
+            return {message : "SUCCESS", car: newCar};
         }catch(err){
+            return err;
+        }
+    }
+    //전체 상품 조회
+    async getAllCarsInfo () {
+        try {
+            const allCars = await Car.find({}, 
+                { carName:1, carPrice:1, img:1, speed:1 ,mileage:1 ,fuel:1 ,carId:1 ,option:1, category:1});
+            if(allCars) {
+                return { message: "SUCCESS", users:allCars };
+            } else {
+                return { message: "NO_CARS", };
+            }
+        } catch(err) {
             return err;
         }
     }
