99e41e0daf39b450769ea9790328ccc7bc941d3d
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-03 16:55:29 +0900

    feat:상품수정 수정

--- routers/carRouter.js
+++ routers/carRouter.js
@@ -3,13 +3,14 @@ import { Car } from '../db/models/cars/cars-model.js';
 import CarService from '../services/car-service.js';
 const carService = new CarService;
 const router = Router();
+//에러처리는 일단 crud 다 쓰고 next(err)로 바꿔보기..
 
 //상품 전체 조회
 router.get('/', async(req,res,next)=>{
     try{
         const result = await carService.getAllCarsInfo();
         if(result.message === "SUCCESS"){
-            res.status(200).json({message:"상품 전체 조회 성공",car:result.cars});
+            res.status(200).json({message:"상품 전체 조회 성공",car: result.car});
             return;
         }else{
             throw {status: 404, message: 'unknown error'};
@@ -24,7 +25,7 @@ router.get('/:carId', async (req, res, next) => {
     try{
         const result = await carService.getCarInfo(carId);
         if(result.message === "SUCCESS"){
-            res.status(200).json({message:"상품 정보 조회에 성공했습니다", car: result.cars});
+            res.status(200).json({message:"상품 정보 조회에 성공했습니다", car: result.car});
             return;
         }else if(result.message === "NO_MATCHES"){
             throw {status:404, message: "존재하지 않는 상품입니다"};
@@ -41,7 +42,7 @@ router.post('/carup', async (req, res, next) => {
     try {
         const result = await carService.CarUp({ carName, carPrice, img, speed, mileage, fuel, option, category });
         if(result.message = "SUCCESS"){
-            res.status(201).json({message:'상품 등록 성공', result});
+            res.status(201).json({message:'상품 등록 성공', car: result.car});
         }else if(result.message === "DUPLICATED"){
             throw {status:400, message: "이미 등록된 상품아이디 입니다"};
         }else if(result.message === "MISSING_FIELD"){
@@ -56,26 +57,20 @@ router.post('/carup', async (req, res, next) => {
 
 
 router.put('/:carId', async (req, res, next) => {
-    const carId = req.params.carId;
-    const { carName, carPrice, img, speed, mileage, option, category } = req.body;
-    try {
-        const carToUpdate = await Car.findOne({ _id: carId });
-        if (!carToUpdate) {
-            return res.status(404).json({ message: 'Car not found' });
+    const {carId} = req.params;
+    const {carName, carPrice, img,speed,mileage,fuel,option,category} = req.body;
+    try{
+        const result = await carService.updateCarInfo(carId, {carName, carPrice, img,speed,mileage,fuel,option,category});
+        if(result.message === "SUCCESS"){
+            res.status(200).json({message:"상품 수정에 성공했습니다", car: result.car});
+            return;
+        }else if(result.message === "NO_MATCHES"){
+            throw {status: 404, message: "존재하지 않는 상품입니다"};
+        }else{
+            throw {status: 404, message: "unkown error"};
         }
-        carToUpdate.carName = carName;
-        carToUpdate.carPrice = carPrice;
-        carToUpdate.img = img;
-        carToUpdate.speed = speed;
-        carToUpdate.mileage = mileage;
-        carToUpdate.option = option;
-        carToUpdate.category = category;
-        await carToUpdate.save();
-
-        res.status(200).json({ message: 'Car updated successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error updating car' });
+    }catch (error) {
+        res.status(err.status).json({message:err.message});
     }
 });
 
