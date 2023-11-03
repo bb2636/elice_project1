222b7ad1c232de92b84d8454e216ca8f3d7477aa
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-03 15:25:04 +0900

    feat:car-service carup수정

--- services/car-service.js
+++ services/car-service.js
@@ -1,8 +1,20 @@
 import { Car } from '../db/models/cars/cars-model.js';
 
 export default class CarService {
-    async CarUp({carName, carPrice, img,speed,mileage,carId,option,category}) {
-        const car = await Car.create({carName, carPrice, img,speed,mileage,carId,option,category});
-        return car;
+    async CarUp({carName, carPrice, img,speed,mileage,fuel,carId,option,category}) {
+        const car = await Car.create({carName, carPrice, img,speed,mileage,fuel,carId,option,category});
+        try{
+            if(!carName || !carPrice || !img || !speed || !mileage || !fuel || !carId){
+                return {message : "Missing_Field"}
+            }
+            const existCar = await Car.findOne({carId: car.carId});
+            if(existCar != null){
+                return {message: "Duplicated"}; //carId중복
+            }
+            const newCar = await Car.create(car);
+            return {message : "Success", car: newCar};
+        }catch(err){
+            return err;
+        }
     }
 }
