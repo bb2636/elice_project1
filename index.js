c7dac3b2132cc81223af7826125179616949059c
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-07 21:16:08 +0900

    CHORE : 배포용 파일 서빙을 위한 준비 작업

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import mainRouter from "./routers/routers.js";
 import cors from "cors";
 import {config} from "dotenv";
 
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 config(); // dotenv 설정 호출
 
 const MONGO_NAME = process.env.MONGO_NAME;
@@ -33,10 +39,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// api router
+app.use("/api", mainRouter);
+
 // static file serving
 app.use("/images", express.static("public/assets/cars/"));
 
-app.use("/api", mainRouter);
+// react build file serving as static
+app.use("/", express.static(join(__dirname, "build")));
+
+// render react app
+// "/" 가 아닌 "*"을 써야 react-router에 설정된 모든 URL에 대해 index.html을 서빙한다.
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`서버가 정상적으로 시작되었습니다. 주소: http://localhost:${port}`);
