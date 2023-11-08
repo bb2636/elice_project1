6c1308c8990ca448cc736e6151661c51c29f21ce
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-08 13:28:41 +0900

    FIX : file upload 시에 변경된 image link 값을 담아 반환

--- routers/upload/upload-router.js
+++ routers/upload/upload-router.js
@@ -14,15 +14,18 @@ const storage = multer.diskStorage({
         cb(null, filename + '-' + Date.now() + extname); // 저장할 파일명을 설정합니다. 이 예제에서는 파일명에 타임스탬프를 추가합니다.
     }
 });
-  
+
 const upload = multer({ storage: storage });
 
 // 업로드 요청
 router.post('/',
     upload.single('file'),
     async(req, res, next) => {
-
-        res.json({message:"success", file: req.file});        
+        const { carName, carPrice, carId, mileage, fule, option, category, color} = req.body;
+        res.json({
+            message:"success",
+            file: req.file,
+            car: { carName, carPrice, carId, mileage, fule, option, category, color, img:`/images/${req.file.filename}`} });        
     }
 );
 
