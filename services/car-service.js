c2fc2d8a2097afcf2e36f2b2e3c77a6d55d67600
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-11-08 14:47:00 +0900

    feat:return값 통일

--- services/car-service.js
+++ services/car-service.js
@@ -5,47 +5,36 @@ export default class CarService {
     async CarUp({carName, carPrice, img,speed,mileage,fuel,carId,option,category, color}) {
 
         const car = {carName, carPrice, img,speed,mileage,fuel,carId,option,category, color};
-        try{
-            const existCar = await Car.findOne({carId: car.carId});
-            if(existCar != null){
-                return {message: "DUPLICATED"}; //carId중복
-            }
-            const newCar = await Car.create(car);
-            return {message : "SUCCESS", car: newCar};
-        }catch(err){
-            throw err;
+        const existCar = await Car.findOne({carId: car.carId});
+        if(existCar != null){
+            throw {message: "DUPLICATED"}; //carId중복
         }
+        const newCar = await Car.create(car);
+            return {message : "SUCCESS", car: newCar};
     }
     //전체 상품 조회
     async getAllCarsInfo() {
-        try {
             const allCars = await Car.find({}, 
                 { carName:1, carPrice:1, img:1, speed:1 ,mileage:1 ,fuel:1 ,carId:1 ,option:1, category:1, color:1});
             if(allCars) {
                 return { message: "SUCCESS", car :allCars };
             } else {
-                return { message: "NO_CARS", };
+                throw { message: "NO_CARS", };
             }
-        } catch(err) {
-            throw err;
-        }
     }
     //상품 상세 조회
     async getCarInfo(carId){
-        try{
-            const matchCar = await Car.findOne(
-                {carId:carId},
-                {carName:1, carPrice:1, img:1, speed:1 ,mileage:1 ,fuel:1 ,carId:1 ,option:1, category:1, color:1});
-            if(matchCar){
-                return {message:"SUCCESS", car: matchCar};
-            }else{ return {message: "NO_MATCHES"};}
-        }catch(err){
-            throw err;
+        const matchCar = await Car.findOne(
+            {carId:carId},
+            {carName:1, carPrice:1, img:1, speed:1 ,mileage:1 ,fuel:1 ,carId:1 ,option:1, category:1, color:1});
+        if(matchCar){
+            return {message:"SUCCESS", car: matchCar};
+        }else{ 
+            throw {message: "NO_MATCHES"};
         }
     }
     //상품 수정
     async updateCarInfo(carId, data){
-        try{
             const updateCar = await Car.findOneAndUpdate(
                 {carId:carId},
                 {carName: data.carName, carPrice:data.carPrice, img: data.img, speed: data.speed, mileage: data.mileage, fuel: data.fuel, option: data.option, category: data.category, color: data.color},
@@ -53,23 +42,16 @@ export default class CarService {
             if(updateCar){
                 return {message: "SUCCESS", car: updateCar};
             }else{
-                return {message: "NO_MATCHES"};
+                throw {message: "NO_MATCHES"};
             }
-        }catch(err){
-            throw err;
-        }
     }
     //상품 삭제
     async deleteCarInfo(carId){
-        try{
-            const deleteCar = await Car.findOneAndDelete({carId:carId});
-            if(deleteCar){
-                return {message: "SUCCESS"};
-            }else{
-                return {message: "NO_MATCHES"};
-            }
-        }catch(err){
-            throw err;
+        const deleteCar = await Car.findOneAndDelete({carId:carId});
+        if(deleteCar){
+            return {message: "SUCCESS"};
+        }else{
+            throw {message: "NO_MATCHES"};
         }
     }
 }
