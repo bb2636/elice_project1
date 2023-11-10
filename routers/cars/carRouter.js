3551dc42c446125d66332013fdc7289dd746d361
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-10 17:50:16 +0900

    FIX : cars get API 에러 처리 수정

--- routers/cars/carRouter.js
+++ routers/cars/carRouter.js
@@ -36,8 +36,6 @@ router.get('/:carId', async (req, res, next) => {
         if(result.message === "SUCCESS"){
             res.status(200).json({message:"상품 정보 조회에 성공했습니다", car: result.car});
             return;
-        }else if(result.message === "NO_MATCHES"){
-            throw {status:404, message: "존재하지 않는 상품입니다"};
         }
     }catch (err) {
         res.status(err.status).json({message:err.message});
